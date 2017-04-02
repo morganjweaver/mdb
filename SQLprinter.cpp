777b482e6df5b176eccf25bceb6c64a96feb4bbd
Morgan Weaver <morganjweaver@gmail.com>
2017-04-02 14:44:19 -0700

    separated print functions into their own file

--- SQLprinter.cpp
+++ SQLprinter.cpp
@@ -3,12 +3,16 @@
 //
 #include<iostream>
 #include "SQLParser.h"
+#include "SQLprinter.h"
+
 
 namespace mdb {
-    void printExpression(const hsql::Expr* expr);
-    void printCreate(const hsql::CreateStatement* stmt);
-    void printSelect(const hsql::SelectStatement* stmt);
-    //std::void printInsert(const hsql::InsertStatement* stmt);
+
+//STILL TO DO?
+//  void printImportStatementInfo(const ImportStatement* stmt, uintmax_t numIndent)
+//  void printInsertStatementInfo(const InsertStatement* stmt, uintmax_t numIndent)
+//  void printStatementInfo(const SQLStatement* stmt) {
+
 
     void Execute(const ::hsql::SQLStatement* result){
         switch (result->type()) {
@@ -18,25 +22,191 @@ namespace mdb {
             case hsql::StatementType::kStmtSelect:
                 printSelect((const hsql::SelectStatement*) result);
                 break;
-//            case hsql::StatementType::kStmtInsert:
-//                printInsert((const hsql::InsertStatement*) result);
-//                break;
             default:
+                std::cout << "Couldn't match statement type";
                 break;
         }
     }
 
-    void printExpression(const hsql::Expr* ex) {
-
-    }
     void printCreate(const hsql::CreateStatement* stmt) {
-        std::cout << "CREATE" << stmt->tableName;
-        std::cout << "filepath test" << stmt->filePath;
+        std::cout << "CREATE TABLE " << stmt->tableName << " ";
+        printf("(");
+        for(int i = 0; i<stmt->columns->size()-1; i++){
+            printColDef(stmt->columns->at(i));
+            printf(", ");
+        }
+        printColDef(stmt->columns->at(stmt->columns->size()-1));
+        printf(")");
     }
+    void printColDef(const hsql::ColumnDefinition* col){
+        printf("%s ", col->name );
+        switch (col->type) {
+            case hsql::ColumnDefinition::UNKNOWN:
+                printf("UNKNOWN");
+                break;
+            case hsql::ColumnDefinition::TEXT:
+                printf("TEXT");
+                break;
+            case hsql::ColumnDefinition::INT:
+                printf("INT");
+                break;
+            case hsql::ColumnDefinition::DOUBLE:
+                printf("DOUBLE");
+                break;
+            default:
+                printf("Unknown type");
+                return;
+        }
+    }
+
     void printSelect(const hsql::SelectStatement* stmt) {
-        std::cout << "SELECT" << "fields test:" << stmt->selectList;
-        std::cout << "sources test:" << stmt->fromTable;
+        std::cout << "SELECT ";
+        for (int i = 0; i < stmt->selectList->size() - 1; i++) {
+            printExpression(stmt->selectList->at(i));
+            printf(", ");
+        }
+        printExpression(stmt->selectList->at(stmt->selectList->size() - 1));
+        printf(" ");
+        printf("FROM ");
+        printTableRef(stmt->fromTable);
+        printf(" ");
+
+        if (stmt->whereClause != NULL) {
+            printf("WHERE ");
+            printExpression(stmt->whereClause);
+            printf(" ");
+        }
+
+        if (stmt->unionSelect != NULL) {
+            printf("UNION ");
+            printSelect(stmt->unionSelect);
+            printf(" ");
+        }
+
+        if (stmt->order != NULL) {
+            printf("ORDER BY ");
+            printExpression(stmt->order->at(0)->expr);
+            printf(" ");
+            if (stmt->order->at(0)->type == hsql::kOrderAsc) printf("ASCENDING ");
+            else printf("DESCENDING ");
+        }
+
+        if (stmt->limit != NULL) {
+            printf("LIMIT ");
+            printf("%" PRId64, stmt->limit->limit);
+            printf(" ");
+        }
     }
+
+    void printExpression(const hsql::Expr* ex){
+
+        switch(ex->type){
+            case hsql::kExprLiteralFloat:
+                printf("%f ", ex->fval);
+                break;
+            case hsql::kExprColumnRef:
+                if(ex->table != NULL){printf("%s.",ex->table);}
+                printf("%s", ex->name);
+                break;
+            case hsql::kExprLiteralString:
+                printf("%s ", ex->name);
+                break;
+            case hsql::kExprLiteralInt:
+                printf("% " PRId64, ex->ival);
+                break;
+            case hsql::kExprStar:
+                printf("* ");
+                break;
+            case hsql::kExprFunctionRef:
+                printf("%s %s ", ex->name, ex->expr->name);
+                break;
+            case hsql::kExprOperator:
+                printOpEx(ex);
+                break;
+            case hsql::kExprSelect:
+            default:
+                printf("Unrecognized expression: %d\n", ex->type);
+                return;
+        }
+        if(ex->alias != NULL){
+            printf("AS %s ", ex->alias);
+        }
+    }
+
+    void printOpEx(const hsql::Expr* ex) {
+        if (ex == NULL) {
+            printf("null");
+            return;
+        }
+        printExpression(ex->expr);
+        printf(" ");
+        switch (ex->opType) {
+            case hsql::Expr::SIMPLE_OP:
+                printf("%c", ex->opChar);
+                break;
+            case hsql::Expr::AND:
+                printf("AND");
+                break;
+            case hsql::Expr::OR:
+                printf("OR");
+                break;
+            case hsql::Expr::NOT:
+                printf("NOT");
+                break;
+            default:
+                printf("%u", ex->opType);
+                break;
+        }
+        printf(" ");
+
+        if(ex->expr2!= NULL) printExpression(ex->expr2);
+    }
+
+    void printTableRef(const hsql::TableRef* tbl){
+        switch (tbl->type) {
+            case hsql::kTableName:
+                printf("%s ", tbl->name);
+                break;
+            case hsql::kTableSelect:
+                printSelect(tbl->select);
+                break;
+            case hsql::kTableJoin:
+
+                printTableRef(tbl->join->left);
+                switch(tbl->join->type) {
+                    case (hsql::kJoinLeft):
+                        printf("LEFT JOIN ");
+
+                        break;
+                    case(hsql::kJoinRight):
+                        printf("RIGHT JOIN ");
+
+                        break;
+                    default:
+                        printf("JOIN ");
+                }
+                printTableRef(tbl->join->right);
+
+                printf("ON ");
+                printExpression(tbl->join->condition);
+                printf(" ");
+                break;
+            case hsql::kTableCrossProduct:
+                for (int i = tbl->list->size()-1; i>0; i--){
+                    printTableRef(tbl->list->at(i));
+                    printf(", ");
+                }
+                printTableRef(tbl->list->at(0));
+                //for (hsql::TableRef* tbl : *tbl->list) printTableRef(tbl);
+                break;
+        }
+        if (tbl->alias != NULL) {
+            printf("AS ");
+            printf("%s ",tbl->alias);
+        }
+    }
+
+
 
 } //namespace mdb
 
--- SQLprinter.h
+++ SQLprinter.h
@@ -1,13 +1,26 @@
 //
 // Created by Morgan Weaver on 4/1/17.
 //
+#ifndef SQL_INTERPRETER_SQLPRINTER_H
+#define SQL_INTERPRETER_SQLPRINTER_H
+
 #include<iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cstdint>
+#include "db_cxx.h"
+#include "database.h"
 #include "SQLParser.h"
 #include "sql/SQLStatement.h"
 
-#ifndef SQL_INTERPRETER_SQLPRINTER_H
-#define SQL_INTERPRETER_SQLPRINTER_H
+
 namespace mdb {
-    std::string Execute(const hsql::SQLStatement* result);
+    void Execute(const ::hsql::SQLStatement* result);
+    void printExpression(const hsql::Expr* ex);
+    void printCreate(const hsql::CreateStatement* stmt);
+    void printSelect(const hsql::SelectStatement* stmt);
+    void printOpEx(const hsql::Expr* ex);
+    void printTableRef(const hsql::TableRef* tbl);
+    void printColDef(const hsql::ColumnDefinition* col);
 }
 #endif //SQL_INTERPRETER_SQLPRINTER_H
